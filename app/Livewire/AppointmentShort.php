28c77b834ededb3cefa234a456737d13c2a073f3
FirmanSyahputra7296 <mhdriski988@gmail.com>
2025-11-25 17:01:02 +0700

    pagination appointment dashboard

--- app/Livewire/AppointmentShort.php
+++ app/Livewire/AppointmentShort.php
@@ -11,19 +11,23 @@ class AppointmentShort extends Component
     use WithPagination;
     public $totalAppointment;
 
+    protected $paginationTheme = 'tailwind';
 
-    public function mount(){
+    public function mount()
+    {
         $this->totalAppointment = Registrasi::count();
     }
 
     public function render()
     {
-        $appointment = Registrasi::with(['pasiens', 'dokters', 'dokter_jadwals'])->orderBy('status', 'asc')->paginate(5)->map(function ($item) {
-            if ($item->tanggal_kunjungan) {
-                $item->tanggal_kunjungan = \Carbon\Carbon::parse($item->tanggal_kunjungan)->format('d M Y');
-            }
-            return $item;
-        });
+        $appointment = Registrasi::with(['pasiens', 'dokters', 'dokter_jadwals'])->orderBy('tanggal_kunjungan', 'asc')->paginate(4)->tap(function ($paginator) {
+                $paginator->getCollection()->transform(function ($item) {
+                    if ($item->tanggal_kunjungan) {
+                        $item->tanggal_kunjungan = \Carbon\Carbon::parse($item->tanggal_kunjungan)->format('d M Y');
+                    }
+                    return $item;
+                });
+            });
 
         return view('livewire.appointment-short', [
             'totalAppointment' => $this->totalAppointment,
--- resources/views/livewire/appointment-short.blade.php
+++ resources/views/livewire/appointment-short.blade.php
@@ -2,16 +2,15 @@
     <h2 class="text-xl font-bold text-gray-800 mb-4">Pendaftaran(Appointments) ({{ $totalAppointment }})</h2>
 
     <div class="overflow-x-auto">
-        <table class="min-w-full border-separate border-spacing-y-3 text-sm">
+        <table class="min-w-full border-separate border-spacing-y-3 text-xs whitespace-nowrap">
             <thead>
                 <tr class="bg-gray-100 text-gray-700">
-                    <th class="px-4 py-2 text-left">Kode</th>
-                    <th class="px-4 py-2 text-left">Tanggal</th>
-                    <th class="px-4 py-2 text-left">Nama Pasien</th>
-                    <th class="px-4 py-2 text-left">Dokter</th>
-                    <th class="px-4 py-2 text-left">Keluhan</th>
-                    <th class="px-4 py-2 text-left">Status</th>
-                    <th class="px-4 py-2 text-center">Aksi</th>
+                    <th class="px-2 py-2 text-left">Kode</th>
+                    <th class="px-2 py-2 text-left">Tanggal</th>
+                    <th class="px-2 py-2 text-left">Nama Pasien</th>
+                    <th class="px-2 py-2 text-left">Dokter</th>
+                    <th class="px-2 py-2 text-left">Keluhan</th>
+                    <th class="px-2 py-2 text-left">Status</th>
                 </tr>
             </thead>
 
@@ -19,18 +18,18 @@
 
                 @foreach ($appointment as $item)
                     <tr class="bg-white shadow rounded-lg hover:bg-gray-50">
-                        <td class="px-4 py-2">{{ $item->appointment_code }}</td>
-                        <td class="px-4 py-2">{{ $item->tanggal_kunjungan }}</td>
-                        <td class="px-4 py-2">{{ $item->pasiens->name }}</td>
-                        <td class="px-4 py-2">{{ $item->dokters->name }}</td>
-                        <td class="px-4 py-2">{{ $item->keluhan }}</td>
-                        <td class="px-4 py-2">{{ $item->status? 'Check-In' : 'Belum' }}</td>
-                        <td class="px-4 py-2 text-center">
-                            <button class="p-2 bg-blue-600 text-white rounded-md hover:bg-blue-700">Detail</button>
-                        </td>
+                        <td class="px-2 py-2">{{ $item->appointment_code }}</td>
+                        <td class="px-2 py-2">{{ $item->tanggal_kunjungan }}</td>
+                        <td class="px-2 py-2">{{ $item->pasiens->name }}</td>
+                        <td class="px-2 py-2">{{ $item->dokters->name }}</td>
+                        <td class="px-2 py-2">{{ $item->keluhan }}</td>
+                        <td class="px-2 py-2 {{ $item->status? 'bg-green-50 text-green-600' : 'bg-red-50 text-red-600' }}">{{ $item->status ? 'Check-In' : 'Belum' }}</td>
                     </tr>
                 @endforeach
             </tbody>
         </table>
+        <div>
+            {{ $appointment->links('pagination::tailwind') }}
+        </div>
     </div>
 </div>
