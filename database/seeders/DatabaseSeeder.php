1c6b593762c8ce0febeb8eefce6ea87cff31ef96
FirmanSyahputra7296 <mhdriski988@gmail.com>
2025-12-07 12:24:15 +0700

    final v22

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -40,6 +40,26 @@ class DatabaseSeeder extends Seeder
             'user_id' => $admin->id,
         ]);
 
+        // Dokter pasti
+        $dokterAdit = User::factory()->create([
+            'username'    => 'Adit Muhammad Prasetya Hutagalung',
+            'email'       => 'sewaalkesmedan@gmail.com',
+            'role'        => 'doctor',
+            'approved'    => true,
+            'approved_at' => now(),
+            'password'    => Hash::make('doktersatu'),
+        ]);
+
+        Dokter::factory()->create([
+            'user_id'      => $dokterAdit->id,
+            'name'         => 'dr. Adit Muhammad Prasetya Hutagalung',
+            'alamat'       => 'Jl. Kemenyan Raya No. 82 P. Simalingkar',
+            'spesialisasi' => 'Dokter Umum',
+            'phone'        => '08116150141',
+            'nik'          => '1271070719040000',
+        ]);
+
+
         $doctorUser = User::factory()->create([
             'username'    => 'doctor',
             'email'       => 'dokter@example.com',
@@ -73,19 +93,19 @@ class DatabaseSeeder extends Seeder
 
         $users = User::factory(30)->create();
 
-        foreach ($users??[] as $user) {
+        foreach ($users ?? [] as $user) {
             switch ($user->role) {
                 case 'user':
                     Pasien::factory()->create(['user_id' => $user->id]);
                     break;
 
-                case 'doctor':
-                    $dokter = Dokter::factory()->create(['user_id' => $user->id]);
-                    DokterAktif::create([
-                        'dokter_id' => $dokter->id,
-                        'aktif'     => false,
-                    ]);
-                    break;
+                // case 'doctor':
+                //     $dokter = Dokter::factory()->create(['user_id' => $user->id]);
+                //     DokterAktif::create([
+                //         'dokter_id' => $dokter->id,
+                //         'aktif'     => false,
+                //     ]);
+                //     break;
 
                 case 'admin':
                 case 'superadmin':
